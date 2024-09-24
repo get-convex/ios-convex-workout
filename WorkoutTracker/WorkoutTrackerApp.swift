590df26022e37a33b5e71e2de237ab66fb889d76
Christian Wyglendowski <christian@dowski.com>
2024-09-24 11:50:08 -0400

    Initial Xcode project structure

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Christian Wyglendowski on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by Christian Wyglendowski on 9/24/24.
+//
+
+import Testing
+@testable import WorkoutTracker
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
